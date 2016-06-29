efef1d488bc51860347fca037599dbcf67e06b0d
dytu0316 <dytu0316@gmail.com>
2016-06-29 13:08:31 -0400

    update bash scripts

--- run_all.sh
+++ run_all.sh
@@ -7,4 +7,7 @@ tmux send-keys -t rettiwt:1 'cd kafka' C-m
 tmux send-keys -t rettiwt:1 'python twitter_api_to_kafka.py' C-m
 tmux new-window -t 2
 tmux send-keys -t rettiwt:2 'cd spark_stream' C-m
-tmux send-keys -t rettiwt:2 './run.sh 3' C-m
+if [ $# -ne 1 ]; then
+	tmux send-keys -t rettiwt:2 './run.sh' C-m
+else
+	tmux send-keys -t rettiwt:2 './run.sh '"$1"'' C-m
